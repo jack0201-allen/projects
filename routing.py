f5e410285783b09ee3eadec10f8294ece159591f
UFO <62587026+jack0201-allen@users.noreply.github.com>
2024-03-05 11:07:27 +0800

    Add files via upload

---
+++ routing.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/data/appInfo/<name>", methods=["GET"])
+def queryDataMessageByName(name):
+    print("type(name) : ", type(name))
+    return "String => {}".format(name)
+
+
+if __name__ == "__main__":
+    app.run()
